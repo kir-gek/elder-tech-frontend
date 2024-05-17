b8783fe14eae7110361c632fd4b2466cab2829f8
kir-gek <kirokronos@gmail.com>
2024-05-17 04:31:15 +0300

    header update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -16,17 +16,21 @@ export const Header: React.FC = () => {
   return (
     <div className="fixed top-0 left-0 right-0 z-50 bg-white flex justify-around items-center h-16 shadow-md">
       <img src={Logo} alt="Логотип" className="h-full" />
-      <NavLink to="/" className="text-xl font-bold text-blue-600">ElderTech</NavLink>
+      <NavLink to="/" className="text-2xl font-bold text-emerald-800">Elder<span className="text-black">Tech</span></NavLink>
       <span className="text-lg font-medium text-gray-700">Версия для слабовидящих</span>
-      <div>
-        <NavLink to="/teacher/my-students" className="text-lg font-medium text-gray-700 hover:text-blue-600">Мои студенты</NavLink>
-      </div>
-      <div>
-        <NavLink to="/teacher/my-courses" className="text-lg font-medium text-gray-700 hover:text-blue-600">Мои курсы</NavLink>
-      </div>
-      <div>
-        <NavLink to="/user/profile" className="text-lg font-medium text-gray-700 hover:text-blue-600">Профиль</NavLink>
-      </div>
+      {isLoggedIn && (
+        <>
+          <div>
+            <NavLink to="/teacher/my-students" className="text-lg font-medium text-gray-700 hover:text-blue-600">Мои студенты</NavLink>
+          </div>
+          <div>
+            <NavLink to="/teacher/my-courses" className="text-lg font-medium text-gray-700 hover:text-blue-600">Мои курсы</NavLink>
+          </div>
+          <div>
+            <NavLink to="/user/profile" className="text-lg font-medium text-gray-700 hover:text-blue-600">Профиль</NavLink>
+          </div>
+        </>
+      )}
       <div>
         {isLoggedIn ? (
           <button
@@ -44,3 +48,5 @@ export const Header: React.FC = () => {
     </div>
   );
 };
+
+export default Header;
