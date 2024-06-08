bee921564f1e8bb752fc990c2ad7911e51cda4e3
kir-gek <kirokronos@gmail.com>
2024-06-09 01:28:07 +0300

    added update ava

--- src/components/User/UserProfile.tsx
+++ src/components/User/UserProfile.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserProfile, selectUserProfile, selectUserLoading, selectUserError } from 'store/userSlice';
 import { IRootState } from 'store/root-reducer';
 import { Modal } from '../Modal/Modal';
 import { EditUserProfileForm } from './EditUserProfileForm';
 import axiosInstance from 'api/axiosConfig';
+import {Preloader} from '../common/Preloader';
 
 export const UserProfile: React.FC = () => {
   const dispatch = useDispatch();
@@ -13,6 +14,7 @@ export const UserProfile: React.FC = () => {
   const error = useSelector((state: IRootState) => selectUserError(state));
   const [isEditModalOpen, setEditModalOpen] = useState(false);
   const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   useEffect(() => {
     dispatch(fetchUserProfile());
@@ -34,21 +36,63 @@ export const UserProfile: React.FC = () => {
     }
   };
 
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedFile(e.target.files[0]);
+    }
+  };
+
+  const handleAvatarUpload = async () => {
+    const userId = localStorage.getItem('currentID');
+    if (!userId) {
+      console.error('User ID is not available in localStorage');
+      return;
+    }
+
+    if (!selectedFile) {
+      alert('Пожалуйста, выберите файл для загрузки');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await axiosInstance.patch(`/users/${userId}/image`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log('Response:', response.data);
+      setSelectedFile(null);
+      fetchAvatar(profile?.image_id!);
+    } catch (error) {
+      console.error('Ошибка при загрузке аватарки:', error);
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-6 bg-white bg-opacity-90 rounded-lg shadow-xl">
-      {loading && <p>Загрузка...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {profile && (
+      {loading ? (
+        <Preloader />
+      ) : error ? (
+        <p className="text-red-500">{error}</p>
+      ) : profile ? (
         <div className="flex items-start space-x-6">
           <div className="flex-shrink-0 flex flex-col items-center mt-4">
-            <img
-              className="h-36 w-36 rounded-full object-cover mb-4"
-              src={avatarUrl || 'https://via.placeholder.com/150'}
-              alt="User avatar"
-            />
+            {avatarUrl ? (
+              <img
+                className="h-36 w-36 rounded-full object-cover mb-4"
+                src={avatarUrl}
+                alt="User avatar"
+              />
+            ) : (
+              <Preloader />
+            )}
+            <input type="file" onChange={handleFileChange} className="mb-2" />
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded text-sm focus:outline-none focus:shadow-outline"
-              onClick={() => alert('Изменить аватар')}
+              onClick={handleAvatarUpload}
             >
               Изменить аватар
             </button>
@@ -97,7 +141,7 @@ export const UserProfile: React.FC = () => {
             </div>
           </div>
         </div>
-      )}
+      ) : null}
       <Modal isOpen={isEditModalOpen} onClose={() => setEditModalOpen(false)}>
         <EditUserProfileForm profile={profile} onClose={() => setEditModalOpen(false)} />
       </Modal>
